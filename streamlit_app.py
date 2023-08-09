35dad570d44c6278294a63a549c1751d4f4fa6f6
Caroline Frasca (Lu) <42614552+carolinedlu@users.noreply.github.com>
2023-08-09 16:32:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,16 +17,26 @@ if uploaded_file is not None:
     # # Can be used wherever a "file-like" object is accepted:
     # dataframe = pd.read_csv(uploaded_file)
     # st.write(dataframe)
-    try: 
-        # Opening file from file path
-        base64_pdf = base64.b64encode(uploaded_file).decode('utf-8')
-    except Exception as e: 
-        raise ValueError(f"Can't encode the file. Details: {e}")
+
+    with open(uploaded_file, "rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
 
     # Embedding PDF in HTML
-    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1200" height="1000" type="application/pdf"></iframe>'
+        # pdf_display = F'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+        pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+        st.markdown(pdf_display, unsafe_allow_html=True)
     # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
+    # st.markdown(pdf_display, unsafe_allow_html=True)
+    # try: 
+    #     # Opening file from file path
+    #     base64_pdf = base64.b64encode(uploaded_file).decode('utf-8')
+    # except Exception as e: 
+    #     raise ValueError(f"Can't encode the file. Details: {e}")
+
+    # # Embedding PDF in HTML
+    # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1200" height="1000" type="application/pdf"></iframe>'
+    # # Displaying File
+    # st.markdown(pdf_display, unsafe_allow_html=True)
 
 # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="100%" height="1000px" type="application/pdf"></iframe>'
 # st.markdown(pdf_display, unsafe_allow_html=True)
